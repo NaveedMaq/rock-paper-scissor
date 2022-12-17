07a3dfe7a2baa402bda5b52279b3212987cfd55f
NaveedMaq <naveedmaqbool30@gmail.com>
2022-12-17 21:28:40 +0530

    feat(script.js): Add playRound() for playing single round
        - Input is case insensitive
        - No input(s) other than Rock(includes Rock, rOck,etc),
        Paper, Scissors is accepted by the function. In which case it will
        return Invalid Choice message

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3);
-    switch(randomInt) {
+    switch (randomInt) {
         case 0: return 'Rock';
         case 1: return 'Paper';
         case 2: return 'Scissors';
         default: return 'Rock';
+    }
+}
+
+function playRound(playerChoice, computerChoice) {
+    let playerChoiceLowerCase = playerChoice.toLowerCase();
+    if (playerChoiceLowerCase !== "rock" && playerChoiceLowerCase !== "paper" && playerChoiceLowerCase !== "scissors") {
+        return `${playerChoice} is not a valid choice`;
+    }
+
+    let computerChoiceLowerCase = computerChoice.toLowerCase();
+    if (playerChoiceLowerCase)
+        if (playerChoiceLowerCase === computerChoiceLowerCase) {
+            return `It's a tie. Both chose ${computerChoice}`;
+        }
+
+    if (
+        playerChoiceLowerCase === "rock" && computerChoiceLowerCase === "scissors" ||
+        playerChoiceLowerCase === "paper" && computerChoiceLowerCase === "rock" ||
+        playerChoiceLowerCase === "scissors" && computerChoiceLowerCase === "paper") {
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
+    } else {
+        return `You Lose! ${computerChoice} beats ${playerChoice}`;
     }
 }
