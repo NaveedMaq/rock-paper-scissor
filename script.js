400b266d396ed24de92647d6bd02ad01c509302f
NaveedMaq <naveedmaqbool30@gmail.com>
2022-12-17 22:04:57 +0530

    feat(script.js) Add game function to play the game 5 times
        - Builds on previous code
        - Will require fine tuning by replacing string return values
            with numerical values
        - Display Result and Score after each round
        - Display Final Winner after game ends

--- script.js
+++ script.js
@@ -10,11 +10,8 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) {
     let playerChoiceLowerCase = playerChoice.toLowerCase();
-    if (playerChoiceLowerCase !== "rock" && playerChoiceLowerCase !== "paper" && playerChoiceLowerCase !== "scissors") {
-        return `${playerChoice} is not a valid choice`;
-    }
-
     let computerChoiceLowerCase = computerChoice.toLowerCase();
+
     if (playerChoiceLowerCase)
         if (playerChoiceLowerCase === computerChoiceLowerCase) {
             return `It's a tie. Both chose ${computerChoice}`;
@@ -29,3 +26,37 @@ function playRound(playerChoice, computerChoice) {
         return `You Lose! ${computerChoice} beats ${playerChoice}`;
     }
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let result;
+    for (let i = 1; i <= 5; i++) {
+        playerChoice = prompt(`Round ${i}: Rock, Paper, Scissor?`);
+        if (playerChoice.toLowerCase() !== "rock" && playerChoice.toLowerCase() !== "paper" && playerChoice.toLowerCase() !== "scissors") {
+            result = `${playerChoice} is not a valid choice`;
+            i--;
+        } else {
+            let computerChoice = getComputerChoice();
+
+            result = playRound(playerChoice, computerChoice);
+            if (result.startsWith("You Win")) {
+                playerScore++;
+            } else if (result.startsWith("You Lose")) {
+                computerScore++;
+            }
+        }
+
+        console.log(result + `\nYou: ${playerScore}. Computer: ${computerScore}`)
+    }
+
+    if (playerScore === computerScore) {
+        console.log(`Final Result: It's a tie. Both scored ${playerScore} points`)
+    } else if (playerScore > computerScore) {
+        console.log(`Final Result: You Win\nYour score: ${playerScore}\nComputerScore: ${computerScore}`)
+    } else {
+        console.log(`Final Result: You Lose\nYour score: ${playerScore}\nComputerScore: ${computerScore}`)
+    }
+}
+
+game()
