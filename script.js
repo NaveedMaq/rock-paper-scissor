dcff2f0b4d2f8a504b2929a902eb87cee5db74d3
NaveedMaq <naveedmaqbool30@gmail.com>
2023-01-24 19:20:46 +0530

    feat(index.html, script.js) Add Score Tracking  - The game now tracks score of each player and updates  the values in the window

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const PLAYER_WON = 1;
 const COMPUTER_WON = -1;
 const TIE = 0;
 
+const PLAYER_WON_MESSAGE = 'Player Won Round!';
+const COMPUTER_WON_MESSAGE = 'Computer Won Round!';
+const TIE_MESSAGE = 'Round Tied!';
+
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3);
     switch (randomInt) {
@@ -31,47 +35,103 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-// function game() {
-//     alert("Open Dev Tools to Play");
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     let resultMessage = "NA";
+function game() {
+    alert("Open Dev Tools to Play");
+    let playerScore = 0;
+    let computerScore = 0;
+    let resultMessage = "NA";
 
-//     for (let i = 1; i <= 5; i++) {
-//         playerChoice = prompt(`Round ${i}: Rock, Paper, Scissors?`);
-//         if(playerChoice === null) {
-//             console.log("You quit the game");
-//             return;
-//         }
-//         if (playerChoice.toLowerCase() !== "rock" && playerChoice.toLowerCase() !== "paper" && playerChoice.toLowerCase() !== "scissors") {
-//             resultMessage = `${playerChoice} is not a valid choice`;
-//             i--;
-//         } else {
-//             let result;
-//             let computerChoice = getComputerChoice();
-//             result = playRound(playerChoice, computerChoice);
+    for (let i = 1; i <= 5; i++) {
+        playerChoice = prompt(`Round ${i}: Rock, Paper, Scissors?`);
+        if(playerChoice === null) {
+            console.log("You quit the game");
+            return;
+        }
+        if (playerChoice.toLowerCase() !== "rock" && playerChoice.toLowerCase() !== "paper" && playerChoice.toLowerCase() !== "scissors") {
+            resultMessage = `${playerChoice} is not a valid choice`;
+            i--;
+        } else {
+            let result;
+            let computerChoice = getComputerChoice();
+            result = playRound(playerChoice, computerChoice);
             
-//             if (result === PLAYER_WON) {
-//                 playerScore++;
-//                 resultMessage = `You Win! ${playerChoice} beats ${computerChoice}`;
-//             } else if (result === COMPUTER_WON) {
-//                 computerScore++;
-//                 resultMessage = `You Lose! ${computerChoice} beats ${playerChoice}`;
-//             } else if(result === TIE) {
-//                 resultMessage = `It's a tie. Both chose ${computerChoice}`;
-//             }
-//         }
+            if (result === PLAYER_WON) {
+                playerScore++;
+                resultMessage = `You Win! ${playerChoice} beats ${computerChoice}`;
+            } else if (result === COMPUTER_WON) {
+                computerScore++;
+                resultMessage = `You Lose! ${computerChoice} beats ${playerChoice}`;
+            } else if(result === TIE) {
+                resultMessage = `It's a tie. Both chose ${computerChoice}`;
+            }
+        }
 
-//         console.log(resultMessage + `\nYou: ${playerScore}. Computer: ${computerScore}`)
-//     }
+        console.log(resultMessage + `\nYou: ${playerScore}. Computer: ${computerScore}`)
+    }
 
-//     if (playerScore === computerScore) {
-//         console.log(`Final Result: It's a tie. Both scored ${playerScore} points`)
-//     } else if (playerScore > computerScore) {
-//         console.log(`Final Result: You Win\nYour score: ${playerScore}\nComputerScore: ${computerScore}`)
-//     } else {
-//         console.log(`Final Result: You Lose\nYour score: ${playerScore}\nComputerScore: ${computerScore}`)
-//     }
-// }
+    if (playerScore === computerScore) {
+        console.log(`Final Result: It's a tie. Both scored ${playerScore} points`)
+    } else if (playerScore > computerScore) {
+        console.log(`Final Result: You Win\nYour score: ${playerScore}\nComputerScore: ${computerScore}`)
+    } else {
+        console.log(`Final Result: You Lose\nYour score: ${playerScore}\nComputerScore: ${computerScore}`)
+    }
+}
 
-// game()
+function uiGame() {
+   
+    let round = 1;
+    let playerScore = 0;
+    let computerScore = 0;
+    const choiceButtons = document.querySelectorAll('button.choice');
+
+    choiceButtons.forEach(btn => btn.addEventListener('click', e => {
+        let playerChoice = e.target.textContent;
+        let computerChoice = getComputerChoice();
+        let result = playRound(playerChoice, computerChoice);
+
+        refreshText('#computerChoice span', computerChoice);
+        refreshText('#playerChoice span', playerChoice);
+        console.log({playerChoice});
+        console.log({computerChoice});
+        console.log({result});
+        
+
+        switch(result) {
+            case PLAYER_WON: refreshText('#banner h2', PLAYER_WON_MESSAGE);
+            playerScore++;
+            break;
+
+            case COMPUTER_WON: refreshText('#banner h2', COMPUTER_WON_MESSAGE)
+            computerScore++;
+            break;
+
+            case TIE: 
+            default: refreshText('#banner h2', TIE_MESSAGE)
+            break;
+        }
+
+        round++;
+        refreshText('div.round span', round);
+        refreshText('.score-board .computer span', computerScore);
+        refreshText('.score-board .player span', playerScore);
+    }));
+  
+}
+
+
+function refreshText(query, text) {
+    document.querySelector(query).textContent = text;
+}
+
+
+function resultString(result) {
+    switch(result) {
+        case PLAYER_WON: return 'Player Won Round!';
+        case COMPUTER_WON: return 'Computer Won Round!';
+        case TIE: 
+        default: return 'Round was draw!';
+    }
+}
+
+uiGame()
